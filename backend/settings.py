4d9ca2586f6967415905cc775092aef47d96a7f5
Toby Chun yu <tobychunyu@TobydeMacBook-Air.local>
2025-02-10 21:11:41 +0000

    Updated setting_2

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
 
-ALLOWED_HOSTS = ['backend.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
