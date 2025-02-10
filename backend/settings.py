f518eafa68cb9cb1c5e05cdd6c69094e5b4a306d
Toby Chun yu <tobychunyu@TobydeMacBook-Air.local>
2025-02-10 21:46:30 +0000

    updated config setting

--- backend/settings.py
+++ backend/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
